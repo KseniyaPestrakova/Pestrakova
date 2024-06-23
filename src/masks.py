c8fce1c68bf60cb6afd071293d0e7bc6e5ff7f76
Kseniya Pestrakova <pestrakowa.k@yandex.ru>
2024-06-23 19:59:35 +0300

    small fixes for PEP8

--- src/masks.py
+++ src/masks.py
@@ -3,16 +3,11 @@ def get_mask_card_number(card_number: int) -> str:
     return str(card_number)[:4] + " " + str(card_number)[4:6] + "** **** " + str(card_number)[-4:]
 
 
-
-
-
 def get_mask_account(account_number: int) -> str:
     """возвращает маску номера счета в формате **XXXX"""
     return "**" + str(account_number)[-4:]
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print(get_mask_account(73654108430135874305))
     print(get_mask_card_number(7000792289606361))
-
-
