e17970c631c03c9684a4996528f0d382de777228
Kseniya Pestrakova <pestrakowa.k@yandex.ru>
2024-06-23 19:12:48 +0300

    add function sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -7,9 +7,13 @@ def filter_by_state(list_dic: list, state='EXECUTED') -> list:
     return result_list
 
 
-def sort_by_date():
+def sort_by_date(list_dic_dates: list, reverse=True) -> list:
     """Функция сортировки операций по дате"""
-    pass
+    if reverse == False:
+        return sorted(list_dic_dates, key=lambda  dic_dates: dic_dates['date'])
+    else:
+        return sorted(list_dic_dates, key=lambda dic_dates: dic_dates['date'], reverse=True)
+
 
 
 if __name__ == '__main__':
@@ -21,3 +25,8 @@ if __name__ == '__main__':
                            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
                            {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
                            {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}], 'CANCELED'))
+    print(sort_by_date([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+                        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+                        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+                        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
+
