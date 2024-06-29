085f837d8c16304bb5717521233b9deb0dae2edf
Kseniya Pestrakova <pestrakowa.k@yandex.ru>
2024-06-29 22:49:49 +0300

    add test for widget.py

--- src/widget.py
+++ src/widget.py
@@ -3,6 +3,8 @@ from src.masks import get_mask_account, get_mask_card_number
 
 def mask_account_card(client_info: str) -> str:
     """Возвращает маску номера карты или счета с частично скрытыми данными"""
+    if type(client_info) != str:
+        raise ValueError('Недостаточно информации о карте/счете')
     if "Счет" in client_info:
         account_number = int(client_info[-20:])
         return str("Счет " + get_mask_account(account_number))
@@ -11,14 +13,12 @@ def mask_account_card(client_info: str) -> str:
         return str(client_info[:-16] + get_mask_card_number(card_number))
 
 
-print(mask_account_card("Счет 64686473678894779589"))
-print(mask_account_card("MasterCard 7158300734726758"))
-print(mask_account_card("Visa Classic 6831982476737658"))
-
 
 def get_data(my_date: str) -> str:
     """Возвращает дату в виде ДД.ММ.ГГГГ"""
+    if len(my_date) <10:
+        raise ValueError('Недостаточно данных для вывода даты')
     return my_date[8:10] + "." + my_date[5:7] + "." + my_date[:4]
 
 
-print(get_data("2018-07-11T02:26:18.671407"))
+
