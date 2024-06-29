fdec7ba23dd7fd7f5068c381226607d416ffc713
Kseniya Pestrakova <pestrakowa.k@yandex.ru>
2024-06-29 21:54:36 +0300

    update func sort_by_date

---
+++ tests/test_masks.py
@@ -0,0 +1,29 @@
+import pytest
+from src.masks import get_mask_card_number
+from src.masks import get_mask_account
+
+
+@pytest.fixture
+def bad_number() -> int:
+    return 12345
+
+@pytest.mark.parametrize('card_number, expected', [(7000792289606361, '7000 79** **** 6361'),
+                                                   (7000792289605555, '7000 79** **** 5555')])
+def test_get_mask_card_number(card_number, expected):
+    assert get_mask_card_number(card_number) == expected
+
+
+def test_get_mask_card_number_invalid_len(bad_number):
+    with pytest.raises(ValueError):
+        get_mask_card_number(bad_number)
+
+
+@pytest.mark.parametrize('account_number, expected', [(73654108430135874305, '**4305'),
+                                                      (73654108430135871234, '**1234')])
+def test_get_mask_account(account_number, expected):
+    assert get_mask_account(account_number) == expected
+
+
+def test_get_mask_account_invalid_len(bad_number):
+    with pytest.raises(ValueError):
+        get_mask_account(bad_number)
