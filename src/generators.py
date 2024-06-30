eda55b9eead9a778215e5419c0341a814dde1dc3
Kseniya Pestrakova <pestrakowa.k@yandex.ru>
2024-07-01 00:00:54 +0300

    add funcs in test_generators.py

--- src/generators.py
+++ src/generators.py
@@ -1,127 +1,31 @@
-def filter_by_currency(transactions: list, currency: str) -> dict:
-    n = 0
-    while True:
-        my_list = list((transact for transact in transactions if transact["operationAmount"]["currency"]["code"] == currency))
-        yield my_list[n]
-        n += 1
+from typing import Generator
 
 
-transactions =     [
-        {
-            "id": 939719570,
-            "state": "EXECUTED",
-            "date": "2018-06-30T02:08:58.425572",
-            "operationAmount": {
-                "amount": "9824.07",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
-            },
-            "description": "Перевод организации",
-            "from": "Счет 75106830613657916952",
-            "to": "Счет 11776614605963066702"
-        },
-        {
-            "id": 142264268,
-            "state": "EXECUTED",
-            "date": "2019-04-04T23:20:05.206878",
-            "operationAmount": {
-                "amount": "79114.93",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
-            },
-            "description": "Перевод со счета на счет",
-            "from": "Счет 19708645243227258542",
-            "to": "Счет 75651667383060284188"
-        },
-        {
-            "id": 873106923,
-            "state": "EXECUTED",
-            "date": "2019-03-23T01:09:46.296404",
-            "operationAmount": {
-                "amount": "43318.34",
-                "currency": {
-                    "name": "руб.",
-                    "code": "RUB"
-                }
-            },
-            "description": "Перевод со счета на счет",
-            "from": "Счет 44812258784861134719",
-            "to": "Счет 74489636417521191160"
-        },
-        {
-            "id": 895315941,
-            "state": "EXECUTED",
-            "date": "2018-08-19T04:27:37.904916",
-            "operationAmount": {
-                "amount": "56883.54",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
-            },
-            "description": "Перевод с карты на карту",
-            "from": "Visa Classic 6831982476737658",
-            "to": "Visa Platinum 8990922113665229"
-        },
-        {
-            "id": 594226727,
-            "state": "CANCELED",
-            "date": "2018-09-12T21:27:25.241689",
-            "operationAmount": {
-                "amount": "67314.70",
-                "currency": {
-                    "name": "руб.",
-                    "code": "RUB"
-                }
-            },
-            "description": "Перевод организации",
-            "from": "Visa Platinum 1246377376343588",
-            "to": "Счет 14211924144426031657"
-        }
-    ]
-
-# usd_transactions = filter_by_currency(transactions, "USD")
-#
-# for _ in range(3):
-#     print(next(usd_transactions)["id"])
-
-
-def transaction_descriptions(transactions):
+def filter_by_currency(transactions: list, currency: str) -> Generator:
+    """Выводит из списка операций по очереди операции, в которых указана соответсвующая валюта"""
     n = 0
     while True:
         my_list = list(
-            (transact["description"] for transact in transactions))
+            (transact for transact in transactions if transact["operationAmount"]["currency"]["code"] == currency)
+        )
         yield my_list[n]
         n += 1
 
 
-descriptions = transaction_descriptions(transactions)
+def transaction_descriptions(transactions: list) -> Generator:
+    """Выводит описание каждой операции из списка операций по очереди"""
+    n = 0
+    while True:
+        my_list = list((transact["description"] for transact in transactions))
+        yield my_list[n]
+        n += 1
 
-for _ in range(2):
-    print(next(descriptions))
 
-
-def card_number_generator(start, stop):
+def card_number_generator(start: int, stop: int) -> Generator:
+    """Генерирует номера карт в формате XXXX XXXX XXXX XXXX в заданном диапазоне"""
     for number in range(start, stop + 1):
         card_number = str(number)
         while len(card_number) < 16:
-            card_number = '0' + card_number
-        formatted_card_number = f'{card_number[0:4]} {card_number[4:8]} {card_number[8:12]} {card_number[12:16]}'
+            card_number = "0" + card_number
+        formatted_card_number = f"{card_number[0:4]} {card_number[4:8]} {card_number[8:12]} {card_number[12:16]}"
         yield formatted_card_number
-
-for card_number in card_number_generator(1, 5):
-    print(card_number)
-
-# 0000 0000 0000 0001
-# 0000 0000 0000 0002
-# 0000 0000 0000 0003
-# 0000 0000 0000 0004
-# 0000 0000 0000 0005
-
-
-
-
