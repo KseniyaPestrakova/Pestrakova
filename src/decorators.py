b2200cbf1f72209a97d569d64df3245476fae8e6
Kseniya Pestrakova <pestrakowa.k@yandex.ru>
2024-07-07 14:52:17 +0300

    small fixes for PEP8

--- src/decorators.py
+++ src/decorators.py
@@ -1,31 +1,43 @@
-def log(filename=None):
-    def decorator(func):
-        def wrapper(*args, **kwargs):
+from typing import Any
+
+
+def log(filename: Any = None) -> Any:
+    def decorator(func: Any) -> Any:
+        def wrapper(*args: Any, **kwargs: Any) -> Any:
             try:
                 result = func(*args, **kwargs)
                 if filename:
-                    with open(filename, 'a') as f:
-                        f.write(f'{func.__name__} {result}\n')
+                    with open(filename, "a") as f:
+                        f.write(f"{func.__name__} {result}\n")
                         return result
                 else:
-                    print(f'{func.__name__} {result}')
+                    print(f"{func.__name__} {result}")
                     return result
             except Exception as e:
                 if filename:
-                    with open(filename, 'a') as f:
-                        f.write(f'{func.__name__} error:{e}. Inputs: {args},{kwargs}\n')
+                    with open(filename, "a") as f:
+                        f.write(f"{func.__name__} error:{e}. Inputs: {args},{kwargs}\n")
                 else:
-                    print(f'{func.__name__} error:{e}. Inputs: {args},{kwargs}\n')
+                    print(f"{func.__name__} error:{e}. Inputs: {args},{kwargs}\n")
 
         return wrapper
+
     return decorator
 
 
-@log() #filename="mylog.txt"
-def my_function(x, y):
+@log()
+def my_function(x: Any, y: Any) -> Any:
     return x + y
 
+
 my_function(1, 7)
-my_function(1, 8)
-my_function(5, 7)
-my_function(5, '7')
+my_function(5, "7")
+
+
+@log(filename="mylog.txt")
+def my_function_file(x: Any, y: Any) -> Any:
+    return x + y
+
+
+my_function(4, 5)
+my_function(5, "7")
