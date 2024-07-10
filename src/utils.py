6c61b5de31fadb107997de684d213bc9fc1ebdb2
Kseniya Pestrakova <pestrakowa.k@yandex.ru>
2024-07-10 22:22:56 +0300

    add func get_list_transactions

---
+++ src/utils.py
@@ -0,0 +1,21 @@
+import json
+import os
+
+
+def get_list_transactions(path: str) -> list:
+    '''Получаем список словарей с данными о финансовых транзакциях'''
+    try:
+        with open(path, 'r', encoding='utf-8') as file:
+            try:
+                list_transactions = json.load(file)
+                return list_transactions
+            except json.JSONDecodeError:
+                list_transactions = []
+                return list_transactions
+    except FileNotFoundError:
+        list_transactions = []
+        return list_transactions
+
+
+
+
