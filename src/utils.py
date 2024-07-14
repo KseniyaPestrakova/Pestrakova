5bb4921941f0b46a5a0968fe7dd6e2119b8e3663
Kseniya Pestrakova <pestrakowa.k@yandex.ru>
2024-07-14 22:47:41 +0300

    add logger masks.py and upload README.md

--- src/utils.py
+++ src/utils.py
@@ -4,29 +4,29 @@ from typing import Any
 
 logging.basicConfig(
     level=logging.INFO,
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    format="%(asctime)s - %(filename)s - %(levelname)s - %(message)s",
     filename="..\\logs\\utils.log",
     filemode="w",
 )
 
-logger = logging.getLogger("utils")
+utils_logger = logging.getLogger("utils")
 
 
 def get_list_transactions(path: str) -> list:
     """Получаем список словарей с данными о финансовых транзакциях"""
-    logger.info("Starting func get_list_transactions")
+    utils_logger.info("Starting func get_list_transactions")
     try:
-        logger.info(f"Open file {path}")
+        utils_logger.info(f"Open file {path}")
         with open(path, "r", encoding="utf-8") as file:
             try:
                 list_transactions: list[Any] = json.load(file)
-                logger.info(f"Return list_transactions from {path}")
+                utils_logger.info(f"Return list_transactions from {path}")
                 return list_transactions
             except json.JSONDecodeError:
-                logger.error("Problems with JSON file")
+                utils_logger.error("Problems with JSON file")
                 list_transactions_error: list[Any] = []
                 return list_transactions_error
     except FileNotFoundError:
-        logger.error(f"File {path} not found")
+        utils_logger.error(f"File {path} not found")
         list_transactions_notfound_error: list[Any] = []
         return list_transactions_notfound_error
