218c348440fff3e229174777b662e8b1f4e3a09e
Kseniya Pestrakova <pestrakowa.k@yandex.ru>
2024-07-20 23:07:09 +0300

    add tests for new funcs

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,8 @@
 import json
 import logging
+import os
 from typing import Any
+
 import pandas as pd
 
 logging.basicConfig(
@@ -34,11 +36,11 @@ def get_list_transactions(path: str) -> list:
 
 
 def get_list_transactions_csv(path: str) -> list:
-    '''Получаем список словарей с данными о финансовых транзакциях из csv-файла'''
+    """Получаем список словарей с данными о финансовых транзакциях из csv-файла"""
     utils_logger.info("Starting func get_list_transactions_csv")
     try:
-        df = pd.read_csv(path, delimiter=';')
-        list_transactions = df.to_dict(orient='records')
+        df = pd.read_csv(path, delimiter=";")
+        list_transactions = df.to_dict(orient="records")
         utils_logger.info(f"Return list_transactions from {path}")
         return list_transactions
     except Exception:
@@ -48,19 +50,14 @@ def get_list_transactions_csv(path: str) -> list:
 
 
 def get_list_transactions_xlsx(path: str) -> list:
-    '''Получаем список словарей с данными о финансовых транзакциях из excel-файла'''
+    """Получаем список словарей с данными о финансовых транзакциях из excel-файла"""
     utils_logger.info("Starting func get_list_transactions_xlsx")
     try:
         df_excel = pd.read_excel(path)
-        list_transactions = df_excel.to_dict(orient='records')
+        list_transactions = df_excel.to_dict(orient="records")
         utils_logger.info(f"Return list_transactions from {path}")
         return list_transactions
     except Exception:
         utils_logger.error(f"Something wrong with excel-file {path}")
         list_transactions = []
         return list_transactions
-
-
-if __name__ == '__main__':
-    result = get_list_transactions_xlsx('..\\data\\transactions_excel.xlsx')
-    print(result)
